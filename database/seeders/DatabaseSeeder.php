a7fdd5545f83c0cf4bca19ba81452407299c0937
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-05-22 17:50:50 +0400

    change requests

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Blog;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,9 +19,10 @@ class DatabaseSeeder extends Seeder
 
         Blog::factory(20)->create();
 
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
+        User::factory()->create([
+            'name' => 'anri',
+            'email' => 'anri@gmail.com',
+            'password' => Hash::make('anri1234')
+        ]);
     }
 }
